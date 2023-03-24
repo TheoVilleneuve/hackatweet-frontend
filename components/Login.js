6cf0afb7fcc6e67ebc55cdd89a892be52a0f69a2
Theo Villeneuve <theovilleneuve.pro@gmail.com>
2023-03-24 16:52:07 +0100

    fin de journee theo

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,59 @@
 import styles from '../styles/Login.module.css';
 import { Modal } from 'antd';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout } from '../reducers/user';
 
 function Login() {
 
+//Gestion USER
+const dispatch = useDispatch();
+const user = useSelector((state) => state.user.value);
+
+const [signUpUsername, setSignUpUsername] = useState('');
+const [signUpUserFirstname, setSignUpUserFirstname] = useState('');
+const [signUpPassword, setSignUpPassword] = useState('');
+const [signInUsername, setSignInUsername] = useState('');
+const [signInPassword, setSignInPassword] = useState('');
+
+//SIGNUP
+const handleRegister = () => {
+  console.log('You better work')
+  fetch('http://localhost:3000/users/signup', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username: signUpUsername, firstname: signUpUserFirstname, password: signUpPassword }),
+  }).then(response => response.json())
+    .then(data => {
+      if (data.result) {
+        dispatch(login({username: signUpUsername, token:data.token}));
+        setSignUpUsername('');
+        setSignUpUserFirstname('');
+        setSignUpPassword('');
+        location.assign('http://localhost:3001/home')
+      }
+    });
+};
+
+//SIGN IN
+const handleConnection = () => {
+  console.log('click Biatch')
+  fetch('http://localhost:3000/users/signin', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username: signInUsername, password: signInPassword }),
+  }).then(response => response.json())
+    .then(data => {
+      if (data.result) {
+        dispatch(login({username: signUpUsername, token:data.token}));
+        setSignInUsername('');
+        setSignInPassword('');
+        location.assign('http://localhost:3001/home')
+      }
+    });
+};
+
+//GESTION DE LA MODALE
   const [isModalUpVisible, setIsModalUpVisible] = useState(false);
   const [isModalInVisible, setIsModalInVisible] = useState(false);
 
@@ -28,7 +78,7 @@ function Login() {
       <h1>Connect to Hackatweet</h1>
       <input className={styles.inputLog} type="text" placeholder="Username" id="username"/>
       <input className={styles.inputLog} type="text" placeholder="Password" id="password"/>
-      <button>Sign in</button>
+      <button onClick={() => handleConnection()}>Sign in</button>
     </div>
   )
 
@@ -37,10 +87,16 @@ function Login() {
     <div className={styles.modaleContainer}>
       <img className={styles.twitterLogoSmall} src={'/logo-twitter.png'} alt='TwitterBackground'/>
       <h1>Create your hacktweet account</h1>
-      <input className={styles.inputLog} type="text" placeholder="User Firstname" id="userFirstname"/>
-      <input className={styles.inputLog} type="text" placeholder="Username" id="username"/>
-      <input className={styles.inputLog} type="text" placeholder="Password" id="password"/>
-      <button>Sign up</button>
+      <input 
+      onChange={(e) => setSignUpUserFirstname(e.target.value)} value={signUpUserFirstname}
+      className={styles.inputLog} type="text" placeholder="User Firstname" id="userFirstname"/>
+      <input
+      onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername}
+      className={styles.inputLog} type="text" placeholder="Username" id="username"/>
+      <input 
+      onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword}
+      className={styles.inputLog} type="text" placeholder="Password" id="password"/>
+      <button onClick={() => handleRegister()}>Sign up</button>
     </div>
   )
 
