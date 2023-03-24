3caa7481e03c47138289133079081a831be60e13
Alexandre <Alexandresossou@gmail.com>
2023-03-24 16:54:41 +0100

    17h00

--- components/HomeCompo.js
+++ components/HomeCompo.js
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/HomeCompo.module.css';
 import LeftPart from '../components/LeftPart';
 import NewTweet from '../components/NewTweet';
-import LastTweet from '../components/LastTweet';
 import Tweet from '../components/Tweet';
 import Trends from '../components/Trends';
 
 function HomeCompo() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/tweets')
+      .then(response => response.json())
+      .then(data => setTweets(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  // Get the last tweet from the tweets array
+  const lastTweet = tweets.length > 0 ? tweets[tweets.length - 1] : null;
+
   return (
     <div className={styles.pageContainer}>
 
@@ -13,15 +25,25 @@ function HomeCompo() {
 
       <div className={styles.midSection}>
         <NewTweet/>
-        <LastTweet/>
+        <div className={styles.pinnedTweetContainer}>
+          {lastTweet && (
+            <Tweet
+              text={lastTweet.text}
+              username={lastTweet.username}
+              createdAt={lastTweet.createdAt}
+              isPinned={true}
+            />
+          )}
+        </div>
         <div className={styles.tweetsContainer}>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
-          <Tweet/>
+          {tweets.map(tweet => (
+            <Tweet
+              key={tweet.id}
+              text={tweet.text}
+              username={tweet.username}
+              createdAt={tweet.createdAt}
+            />
+          ))}
         </div>
       </div> 
 
--- components/Tweet.js
+++ components/Tweet.js
@@ -1,26 +1,41 @@
 import styles from '../styles/Tweet.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
 
+function Tweets() {
+  const [tweet, setTweet] = useState(null);
 
-function Tweet() {
+  useEffect(() => {
+    async function fetchTweet() {
+      const tweetData = await tweet.findOne().exec();
+      setTweet(tweetData);
+    }
 
-    return (
-        <div className={styles.LastTweetContainer}>
-            <div className={styles.userContainer}>
-                <img className={styles.userLogo} src={'/userLogo.jpeg'} alt='TwitterBackground'/>
-                <div className={styles.userInfos}>
-                    <p className={styles.userFirstname}>Antoine</p>
-                    <p className={styles.userName}>@AntoineLeProf <span className={styles.hourOfPost}>5 hours</span></p>
-                </div>
-            </div> 
-            <p className={styles.tweet}>Ceci est un ancien tweet</p>  
-            <div className={styles.tweetLikeContainer}>
-                <FontAwesomeIcon icon={faHeart} onClick={() => handleLikeTweet()} className={styles.like} />
-                <span className={styles.likeCounter}>(3)</span>
-            </div>
-        </div>
-    );
+    fetchTweet('http://localhost:3000/tweets');
+  }, []);
+
+  if (!tweet) {
+    return <p>No tweet found</p>;
   }
+
+  return (
+    <div className={styles.LastTweetContainer}>
+      <div className={styles.userContainer}>
+        <img className={styles.userLogo} src={'/userLogo.jpeg'} alt='TwitterBackground'/>
+        <div className={styles.userInfos}>
+          <p className={styles.userFirstname}>{tweet.author}</p>
+          <p className={styles.userName}>@{tweet.author} <span className={styles.hourOfPost}>5 hours</span></p>
+        </div>
+      </div> 
+      <p className={styles.tweet}>{tweet.tweetContent}</p>  
+      <div className={styles.tweetLikeContainer}>
+        <FontAwesomeIcon icon={faHeart} onClick={() => handleLikeTweet()} className={styles.like} />
+        <span className={styles.likeCounter}>(3)</span>
+      </div>
+    </div>
+  );
+}
+
   
-  export default Tweet;
+  export default Tweets;
